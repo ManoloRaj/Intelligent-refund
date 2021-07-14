8216c477b1c80ddeaa311c297fbe43fef5460abf
Manolo Rajaonah <77987911+ManoloRaj@users.noreply.github.com>
2021-07-14 17:39:09 +0300

    Add files via upload
    refresh refund function

--- program.py
+++ program.py
@@ -1,1 +1,12 @@
-print("DEBUT")
+class Money:
+    def __init__(self):
+        self.list_of_money = [10, 20, 50, 100, 1000, 2000, 5000, 10000, 20000]
+
+    def getRestOfMoney(self, money, price) :
+        return price-money 
+    
+    def compute(self, total):
+        return total
+
+
+
