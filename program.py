57abfb700936a646f295c2728b310ab2b91b6bce
Manolo Rajaonah <77987911+ManoloRaj@users.noreply.github.com>
2021-07-14 17:42:12 +0300

    Add files via upload

--- program.py
+++ program.py
@@ -1,11 +1,14 @@
 class Money:
     def __init__(self):
         self.list_of_money = [10, 20, 50, 100, 1000, 2000, 5000, 10000, 20000]
-
+        #Initialise refun to 0
+        self.refund = 0
     def getRestOfMoney(self, money, price) :
+        self.refund = price-money
         return price-money 
     
     def compute(self, total):
+        totale - self.list_of_money[:]
         return total
 
 
