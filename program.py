1c8d5e3a628f052960a4664f02fbc7473c8550f2
Manolo Rajaonah <77987911+ManoloRaj@users.noreply.github.com>
2021-07-14 21:59:19 +0300

    Add files via upload

--- program.py
+++ program.py
@@ -1,15 +1,20 @@
 class Money:
     def __init__(self):
-        self.list_of_money = [10, 20, 50, 100, 1000, 2000, 5000, 10000, 20000]
+        self.list_of_money = [10, 20, 50, 100, 500, 1000, 2000, 5000, 10000, 20000]
         #Initialise refun to 0
         self.refund = 0
     def getRestOfMoney(self, money, price) :
         self.refund = price-money
-        return price-money 
+        return money-price 
     
-    def compute(self, total):
-        total - self.list_of_money[:]
-        return total
+    def computeIntelligentRest(self, money):
+        money = []
+        for count in range(1,len(self.list_of_money)):
+            money = total - self.list_of_money[count]
+        return money
 
+if __name__ == "__main__":
+    money = Money()
+    print(money.getRestOfMoney(700, 67))
 
 
