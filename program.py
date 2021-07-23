2806143d0810e002c68942f4512d040a900d5001
Manolo Rajaonah <77987911+ManoloRaj@users.noreply.github.com>
2021-07-23 23:14:50 +0300

    Update program.py

--- program.py
+++ program.py
@@ -3,8 +3,10 @@
 class Money:
     def __init__(self):
         self.list_of_money = [10, 20, 50, 100, 500, 1000, 2000, 5000, 10000, 20000]
+        
         #Initialise refun to 0
         self.refund = 0
+        
     def getRestOfMoney(self, money, price) :
         self.refund = price-money
         return money-price 
@@ -13,6 +15,10 @@ class Money:
         money = []
         for count in range(1,len(self.list_of_money)):
             money = total - self.list_of_money[count]
+            if money % self.list_of_money[count] == 0
+                self.refund = self.refund + self.list_of_money[count]
+            else :
+                continue
         return money
 
 
